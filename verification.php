6ddc3ea7bafaed7c6cb969a281238164fa0d1e89
hynnah <hannahmarie.martinez.12@gmail.com>
2025-06-22 13:41:57 +0000

    Update Login and added Signup, as well as other relevant files

---
+++ verification.php
@@ -0,0 +1,23 @@
+<?php
+    if (in_array($_SERVER['SERVER_NAME'], ['localhost', '127.0.0.1'])) {
+    // LOCALHOST
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "foundit";
+} else {
+    // ONLINE
+    $servername = "localhost"; 
+    $username = "s22102131_foundit";
+    $password = "fi_database123";
+    $dbname = "s22102131_foundit";
+}
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection Failed! " . $conn->connect_error);
+}
+
+
+?>
